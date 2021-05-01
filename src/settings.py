c7713007b21cd7d8f624271a60cb1feb63f2347c
Giorgos Stamatelatos <gstamatelat@gmail.com>
2021-05-01 20:39:14 +0300

    Fix default xelatex value

--- src/settings.py
+++ src/settings.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description='Vectorized Antichamber moral wall.
 parser.add_argument('--magick', type=str, help='location of the magick executable', default='magick')
 parser.add_argument('--potrace', type=str, help='location of the potrace executable', default='potrace')
 parser.add_argument('--pdflatex', type=str, help='location of the pdflatex executable', default='pdflatex')
-parser.add_argument('--xelatex', type=str, help='location of the xelatex executable', default='pdflatex')
+parser.add_argument('--xelatex', type=str, help='location of the xelatex executable', default='xelatex')
 parser.add_argument('--pdfinfo', type=str, help='location of the pdfinfo executable', default='pdfinfo')
 parser.add_argument('--image-size', type=float, help='size in cm of each sign', default=1)
 parser.add_argument('--quote-ratio', type=float, help='width to height ratio of each quote', default=3.75)
